30f48b6d4bdfd95865d65b07b1b0800851529f56
Estefan Hense <tefagh@hotmail.com>
2020-09-16 03:15:54 -0300

    adjusting db name

--- crudEstabelecimentos/Model/BD.class.php
+++ crudEstabelecimentos/Model/BD.class.php
@@ -3,12 +3,12 @@
  class BD {
 	
     private $host = "localhost";
-    private $banco = "tcc";
+    private $banco = "iplayq25_tcc";
 
  
 	public function __construct() {
 			try {
-				 $this->pdo = new PDO("mysql:host=localhost; dbname=tcc",'root','');
+				 $this->pdo = new PDO("mysql:host=localhost; dbname=iplayq25_tcc",'root','');
 				 $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	 
 			} catch (PDOException  $e) {
