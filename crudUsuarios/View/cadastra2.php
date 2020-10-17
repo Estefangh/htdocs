e0019f852e9656d744d131633397b094d72c7f67
Estefan Hense <estefan.hense@possible.com>
2020-10-17 16:13:56 -0300

    finalizei o front da pagina de cadastro de usuarios, mas o cadastro nao esta funcionando

--- crudUsuarios/View/cadastra2.php
+++ crudUsuarios/View/cadastra2.php
@@ -14,7 +14,67 @@
             unset($_SESSION['msg']); 
         }
     ?>
-
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/index.html">IPlay</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSite">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+      <div class="collapse navbar-collapse" id="navbarSite">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+              <a class="nav-link" href="/index.html">Home</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="/index.html#services">Serviços</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="/index.html">Agendamento</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="/index.html#contact">Contato</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="crudEstabelecimentos/view/cadastra.php">Possui um centro esportivo?</a>
+          </li>
+          <li class="nav-item" data-toggle="modal" data-target="#modalLogin" data-whatever="@getbootstrap">
+              <a class="nav-link active" href="#">Cadastro</a>
+          </li>
+<!--<button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#modalLogin" data-whatever="@getbootstrap">Login</button>-->
+          <div class="modal fade" id="modalLogin" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Login</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                  <div class="modal-body">
+                  <!--  <form name="login" action="../tcc/crudUsuarios/Controller/usuario.php" method="post"> -->
+                  <!--  <form name="login" action="../crudUsuarios/Controller/usuario.php" method="post">--> 
+                      <form name="login" action="../crudUsuarios/Controller/usuario.php" method="post"> 
+                        <div class="form-group">
+                          <label for="usuario" class="col-form-label">Nome de usuário ou e-mail:</label>
+                          <input type="text" name="login" class="form-control" id="user" required>
+                        </div>
+                        <div class="form-group">
+                          <label for="senha" class="col-form-label">Senha:</label>
+                          <input type="password" name="senha" class="form-control" id="senha" required>
+                        </div>
+                        <div class="row" class="modal-footer">
+                        <div class="col-auto mr-auto">  <p><a href="../crudUsuarios/view/cadastra.php">Não possui uma conta? Cadastre-se</a> </div>  
+                        <div class="col-auto"> <button type="submit" name="acao" value="Logar" class="btn btn-secondary">Entrar</button> </div>
+                        <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> -->
+                        </div>
+                    </form>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </ul>
+      </div>
+     </nav>
     <div class="container">
         <div class="row">
             <div class="col-12 text-center my-5">
@@ -72,6 +132,11 @@
                 </form>
             </div>
         </div>
+    </div>
+    <div class="copyright py-3 text-center text-black">
+      <div class="container">
+        <small>Copyright &copy; Estefan Hense 2020</small>
+      </div>
     </div>
 </body>
 </html>
